c05fa54b2e22315ff98e7d12bd1d61fdec645a19
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-05-13 21:52:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr
+    .filter((arr) => arr.marks > 50)
+    .map((arr) => console.log(arr));
+  
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+   arr.forEach((student) => {
+    if (arr.marks > 50) {
+      console.log(arr);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newarr = { id: 4, name: "susan", age: "20", marks: 45 };
+  arr.push(newarr);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  console.log("\nRemoving failed students:");
+  arr = arr.filter((arr) => arr.marks >= 50);
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+   let newStudents = [
+    { id: 5, name: "jane", age: "21", marks: 75 },
+    { id: 6, name: "jim", age: "19", marks: 90 },
+    { id: 7, name: "jessica", age: "18", marks: 60 },
+  ];
+  arr = arr.concat(newStudents);
+  console.log(arr);
 }
